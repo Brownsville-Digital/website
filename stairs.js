245f2ce76fa279c7d9f02165628a4de4d2586958
mikeill <mike@mzoo.org>
2023-06-01 20:39:19 -0500

    Remove old commented out code.

--- stairs.js
+++ stairs.js
@@ -1,47 +1,6 @@
-
-console.log("stairs.js loaded");
+// Desc: An experiment in animating SVG with CSS
 import {choc, set_content, on, DOM} from './factory.js';
 const {"svg:a": SVGA, "svg:g": GROUP, "svg:circle": CIRCLE, "svg:path": PATH, "svg:svg": SVG, "svg:text": SVGTEXT} = choc; //autoimport
-/* const walkingPerson = document.createElement('svg');
-walkingPerson.setAttribute("viewBox", "0 0 1600 900");
-walkingPerson.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-walkingPerson.style.width = "800px";
-walkingPerson.style.height = "450px";
-walkingPerson.style.border = "1px solid black";
-walkingPerson.style.stroke = 'black';
-walkingPerson.style.strokeWidth = '3px';
-walkingPerson.style.fill = 'none';
-const personHead = document.createElement('circle');
-personHead.style.fill = 'none';
-personHead.style.cx = '800';
-personHead.style.cy = '100';
-personHead.style.r = '40';
-personHead.style.display = 'block';
-const torso = document.createElement('path');
-torso.style.stroke = 'black';
-torso.setAttribute('d', 'M 800,140 v 120');
-const stairs = document.createElement('path');
-stairs.setAttribute('name', 'stairs');
-stairs.setAttribute('d', 'M 0,100 h 100, v 100 h 100, v 100 h 100, v 100 h 100, v 100 h 100, v 100 h 100, v 100 h 100, v 100 h 100, v 100');
-const btn = document.createElement('button');
-btn.id = 'btn';
-btn.innerText = 'Click me';
-btn.style.display = 'block';
-btn.onclick = (e) => {
-  console.log(e.target);
-};
-setTimeout(function(){
-  stairs.setAttribute('transform', "translate(0,100) rotate(0.5)");
-  stairs.setAttribute('stroke', "green");
-  stairs.setAttribute('name', "escalator");
-    console.log({"stairs": stairs.getAttribute('name')});
-    setTimeout( () => {
-        stairs.removeAttribute('transform');
-    }, 500)
-}, 2500);
-walkingPerson.append(personHead, torso, stairs);
-main.appendChild(btn);
-main.appendChild(walkingPerson); */
 
 function renderWalkingPerson(node) {
 
